0c21675a753a4c0427854d67a24588e07f0eb932
shirdonliao <toingart@163.com>
2018-12-06 14:58:58 +0800

    调试

--- test.go
+++ test.go
@@ -21,16 +21,17 @@ func main()  {
 	//	//fmt.Println(user)
 	//	//fmt.Println(uid)
 
-	animals := []oop.Animal{oop.Cat{}, oop.Dog{},oop.Dock{}}
-	for _, animal := range animals {
-		fmt.Println(animal.Speak())
-	}
+	//animals := []oop.Animal{oop.Cat{}, oop.Dog{},oop.Dock{}}
+	//for _, animal := range animals {
+	//	fmt.Println(animal.Speak())
+	//}
+	//
+	//oop.Test("string")
+	//oop.Test(123)
+	//oop.Test(true)
 
-	oop.Test("string")
-	oop.Test(123)
-	oop.Test(true)
-
-	names := []string{"a", "0", "c"}
+	names := []string{"a", "0", "c","DDD"}
+	fmt.Println(len(names))
 	vals := make([]interface{}, len(names))
 	for i, v := range names {
 		vals[i] = v
