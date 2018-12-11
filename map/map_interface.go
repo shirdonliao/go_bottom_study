0d49c18e619e97194bcb1756b7d568bc4585aa3e
shirdonliao <toingart@163.com>
2018-12-11 15:53:44 +0800

    research commit

--- map/map_interface.go
+++ map/map_interface.go
@@ -18,7 +18,7 @@ import "fmt"
 //这段代码声明了一个空接口的slice，这意味着它的值可以是任意类型，然后我们声明了两个map，一个是map[string]string，一个是map[string]int，
 //然后在声明一个map的map类型，将这三个类型赋值给slice，使得slice可以存贮各种不同类型的数据，想想看，一个可变数组中，存储了一个key为string类型，
 //value为int类型的map，又存储了一个key为string类型，value为string类型的map，还存储了一个map的map，这对c/c++转go的程序员们来说是多么让人吃惊。
-//空interface没有实现任何方法，可以存储任何类型！
+
 
 func main()  {
 	slice := make([]interface{}, 10)
@@ -28,10 +28,20 @@ func main()  {
 	map2["TaskID"] = 1
 	map3 := make(map[string]map[string]string)
 	map3["mapValue"] = map1
+
+	outMap := make(map[string]interface{},5)
 	slice[0] = map2
 	slice[1] = map1
 	slice[3] = map3
-	fmt.Println(slice[0])
-	fmt.Println(slice[1])
-	fmt.Println(slice[3])
+
+	outMap["a"] = map1
+	outMap["b"] = map2
+	outMap["c"] = map3
+	//fmt.Println(slice[0])
+	//fmt.Println(slice[1])
+	//fmt.Println(slice[3])
+
+	fmt.Println(outMap["a"])
+	fmt.Println(outMap["b"])
+	fmt.Println(outMap["c"])
 }
